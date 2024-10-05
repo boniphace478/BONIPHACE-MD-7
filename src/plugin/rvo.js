7d5d1cc078fa7d8a660d488c873cfb775389bf10
Boniphace Tech <anthonyboniphace478@gmail.com>
2024-10-05 22:28:52 +0300

    Update rvo.js

--- src/plugin/rvo.js
+++ src/plugin/rvo.js
@@ -23,7 +23,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
     const type = Object.keys(msg)[0];
     
     const originalCaption = msg[type].caption || '';
-    const newCaption = `${originalCaption}\n\n> © Powered By Mercedes`;
+    const newCaption = `${originalCaption}\n\n> © Powered By Boniphace`;
 
 
     // Download the media content
